5b26b3147959830eb6c07db9275f7c3127c96e87
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-14 11:07:54 -0500

    Import food script; have not setup on resupply nodes

---
+++ Assets/Scrpit/FoodRes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodRes : MonoBehaviour
+{
+    public GameObject FoodImage;
+    public float SpawnTime;
+    public float timer;
+    public bool taken, StartTimer;
+
+
+    void Update()
+    {
+       
+        if(taken == true)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        if(timer >= SpawnTime)
+        {
+            for (int a = 0; a < transform.childCount; a++)
+            {
+                transform.GetChild(a).gameObject.SetActive(true);
+            }
+            taken = false;
+            timer = 0;
+        }
+    }
+
+    public void setfood()
+    {
+        for (int a = 0; a < transform.childCount; a++)
+        {
+            transform.GetChild(a).gameObject.SetActive(false);
+        }
+        taken = true;
+    }
+
+}
