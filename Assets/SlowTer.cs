25127b84bdcc5e83695cc97e2abc8621e0dc8a73
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-21 16:15:06 -0500

    WARN BUGGY import of Erjun Work via Package; enemies DO NOT SPAWN; ground effects OK; some enemies BROKEN; music missing/bugs out in some; some work to do -- SEE GAME MANAGER FOR DETAILS

---
+++ Assets/SlowTer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float SlowSpeed = 3f;
+    private float PlayerOriginSpeed = 5f;
+    public PlayerController PC;
+    public bool slowed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && slowed == false)
+        {
+            PlayerOriginSpeed = PC.speed;
+           PC.speed = SlowSpeed;
+            slowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Debug.Log(PlayerOriginSpeed);
+       
+        if (collision.tag == "Player")
+        {
+            Debug.Log(PlayerOriginSpeed);
+           PC.speed = PlayerOriginSpeed;
+            slowed = false;
+        }
+    }
+}
