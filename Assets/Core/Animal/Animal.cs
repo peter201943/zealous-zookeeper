8fb6b9cb777630b634be923b749234974ec2e1ad
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-21 20:00:07 -0500

    MUCH changes to enemy AI

--- Assets/Core/Animal/Animal.cs
+++ Assets/Core/Animal/Animal.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 
@@ -32,10 +33,10 @@ public enum AnimalState
 /// * Detection (Of Player)
 /// * Death
 /// * State Management (Via Animation)
+/// <see href="https://www.youtube.com/watch?v=W-NIYi1t16Q">Simple 2D Pathfinding In 4 Lines Of Code! | Unity Tutorial</see>
 /// </summary>
 [RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(Collider2D))]
-[RequireComponent(typeof(AnimalBehavior))]
 public class Animal : MonoBehaviour
 {
 
@@ -53,24 +54,29 @@ public class Animal : MonoBehaviour
 
     [Header("Collision")]
     public Collider2D attackCircle;     // Trigger:  attack when in range
-    
-    // TODO Move to AnimalBehavior and handle from there
-    //public Collider2D detectionCircle;  // Trigger:  change state when in range
-    
-    // IMPLIED
-    // public RigidBody rigidBody;      // Collider: stop on walls
+
+
+    // REMOVED, no sensory code in use
+    // // TODO Move to AnimalBehavior and handle from there
+    // // public Collider2D detectionCircle;  // Trigger:  change state when in range
+
+    // NOT IN USE
+    // // IMPLIED
+    // // public RigidBody rigidBody;      // Collider: stop on walls
 
     [Header("Delays")]
     public float moveSoundDelay;        // How frequently to play the move sound
     public float attackDelay;           // How quickly to attack
     public float spawnDelay;            // How long until start playing
     public float deathDelay;            // How long until deletion
+    public float navDelay = 1.0f;       // How frequently we update our path
 
     // Delay Trackers
     private float currentMoveSoundDelay;
     private float currentAttackDelay;
     private float currentSpawnDelay;
     private float currentDeathDelay;
+    private float currentNavDelay;
 
     // Sounds
     private AudioSource sounds;
@@ -78,17 +84,21 @@ public class Animal : MonoBehaviour
     // States
     private AnimalState animalState;
 
-    // Delegates
-    // We delegate the AnimalBehavior for controlling
-    // * Patrols * Attacks
-    private AnimalBehavior animalBehavior;
+    // NOT IN USE
+    // // Delegates
+    // // We delegate the AnimalBehavior for controlling
+    // // * Patrols * Attacks
+    // private AnimalBehavior animalBehavior;
 
     // What we attack when we find it
-    private GameObject target;
+    private Transform target;
+
+    // What we move with
+    private NavMeshAgent navAgent;
 
 
 
-
+    /// <see href="https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html">GameObject.FindGameObjectsWithTag</see>
     void Start()
     {
         // Sounds
@@ -98,16 +108,26 @@ public class Animal : MonoBehaviour
         // States
         animalState = AnimalState.Waiting;
 
-        // Delegates
-        animalBehavior = GetComponent<AnimalBehavior>();
+        // NOT IN USE
+        // // Delegates
+        // animalBehavior = GetComponent<AnimalBehavior>();
 
         // Timers
         currentMoveSoundDelay   = 0.0f;
         currentAttackDelay      = 0.0f;
         currentSpawnDelay       = spawnDelay;
         currentDeathDelay       = deathDelay;
+        currentNavDelay         = navDelay;
 
-        // TEMP HACK DO NOT USE PERMANENTLY
+        // NOT IN USE?
+        // // TEMP HACK DO NOT USE PERMANENTLY
+
+        // (Try to) Find the Player at Startup
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        if (players.Length > 0)
+        {
+            target = players[0].transform;
+        }
     }
 
 
@@ -115,6 +135,7 @@ public class Animal : MonoBehaviour
 
     void Update()
     {
+        // Slowly decrement all timers
         DecrementAllTimers();
 
         // Manage State
@@ -158,12 +179,34 @@ public class Animal : MonoBehaviour
                 animalState = AnimalState.Patrolling;
             }
         }
+
+        // Look for player (if missing)
+        if (!target)
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            if (players.Length > 0)
+            {
+                target = players[0].transform;
+            }
+        }
+
+        // Update Path every once in a while
+        if (currentNavDelay <= 0.0f)
+        {
+            // Find New Path
+            navAgent.SetDestination(target.position);
+
+            // Reset Timer
+            currentNavDelay = navDelay;
+        }
     }
 
 
 
 
-
+    /// <summary>
+    /// Resets al lof the timers until they hit zero
+    /// </summary>
     private void DecrementAllTimers()
     {
         // MoveSound
@@ -186,19 +229,54 @@ public class Animal : MonoBehaviour
         {
             currentDeathDelay -= Time.deltaTime;
         }
+        // Nav Delay
+        if (currentNavDelay > 0.0f)
+        {
+            currentNavDelay -= Time.deltaTime;
+        }
     }
 
 
 
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // NOT IN USE
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     // If collision is player and we are the awareness circle and we are patrolling
+    //     // chase them
+    //     
+    //     // If collision is player and we are the attack circle and attack delay is done
+    //     // attack them
+    // }
+
+
+
+
+
+    /// <summary>
+    /// When enemy bumps into player
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        // If collision is player and we are the awareness circle and we are patrolling
-        // chase them
-        
         // If collision is player and we are the attack circle and attack delay is done
-        // attack them
+        if (collision.gameObject.tag == "Player" && currentAttackDelay <= 0.0f)
+        {
+            try
+            {
+                // attack them
+                collision.gameObject.GetComponent<PlayerMover>().Damage(attackDamage);
+
+                // reset timer
+                currentAttackDelay = attackDelay;
+            }
+            catch
+            {
+                Debug.Log("Could not attack player");
+            }
+        }
+
     }
 
 
@@ -235,25 +313,26 @@ public class Animal : MonoBehaviour
 
 
 
-    /// <summary>
-    /// Called from update
-    /// Find paths
-    /// TODO
-    /// TODO move to AnimalBehavior
-    /// </summary>
-    private void Patrol()
-    {
+    // NOT IN USE
+    // /// <summary>
+    // /// Called from update
+    // /// Find paths
+    // /// TODO
+    // /// TODO move to AnimalBehavior
+    // /// </summary>
+    // private void Patrol()
+    // {
+    // 
+    // }
 
-    }
 
 
-
-
-    /// <summary>
-    /// TODO move to AnimalBehavior
-    /// </summary>
-    private void Chase()
-    {
-
-    }
+    // NOT IN USE
+    // /// <summary>
+    // /// TODO move to AnimalBehavior
+    // /// </summary>
+    // private void Chase()
+    // {
+    // 
+    // }
 }
